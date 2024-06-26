8d48fc5a7b452501275dc87f8008e9c0d950ddc8
arnold <arnold.reinders@minbzk.nl>
2024-06-26 20:21:58 +0200

    Added generate-delivery

--- src/generate-delivery.py
+++ src/generate-delivery.py
@@ -25,7 +25,7 @@ config_dict = dc.read_config(args.project)
 delivery_filename = args.delivery
 
 # create directories
-db_szervers = config_dict['SERVER_CONFIGS']
+db_servers = config_dict['SERVER_CONFIGS']
 root_dir = config_dict['ROOT_DIR']
 project_dir = config_dict['PROJECT_DIR']
 config_dir = os.path.join(project_dir, 'config')
@@ -39,6 +39,7 @@ lrp = dc.load_odl_table(
 lrp = lrp.set_index('leverancier_kolomtype')
 
 # ask for supplier and project
+print('')
 supplier = ask('Supplier: ', None)
 project = ask('Project: ', None)
 
@@ -51,41 +52,58 @@ meta_files = [f for f in os.listdir(schema_dir)
     and 'meta.csv' in f]
 
 # consider the first file as the meta file
-meta_name = meta_files[0]
+meta_name = os.path.join(schema_dir, meta_files[0])
 meta_data = pd.read_csv(meta_name, sep = ';')
-periode = meta_data.loc[0, 'bronbestanmd_frerquentielevering']
+meta_data = meta_data.set_index('attribuut')
+
+periode = meta_data.loc['Bronbestand frequentielevering', 'waarde']
+
 if periode in ['A', 'I']:
-    print('Wewrkt niet voor levring_rapportageperiode is A of I')
+    print('Werkt niet voor levering_rapportageperiode A of I')
     sys.exit()
 
-if periode = 'J':
+if periode == 'J':
     prd0 = 1
     prd1 = 1
+
 else:
-    range = meta_data.loc[0, 'domein']
-    dmns = range.split(':')
+    bereik = lrp.loc[periode, 'domein']
+    dmns = bereik.split(':')
     dmn0 = int(dmns[0])
-    dmn1 = int(dmns[1])
+    dmn1 = int(dmns[1]) + 1
 
 # read the snippet file
-snippet = os.path.join(config_dir, 'snippet-generate-delivery.yaml')
-snippet = snippet.format(**project)
+snippet_name = os.path.join(config_dir, 'snippet-generate-delivery.yaml')
+with open(snippet_name, 'r') as infile:
+    snippet = infile.read()
 
-years = ask('Years: ', None).split('_')
+# ask which years to process
+years = ask('Years: ', None).split('-')
 year_start = int(years[0])
 if len(years) == 0:
     year_end = year_start
 else:
     year_end = int(years[1])
 
-for year in range(year_start to year_end):
-    for period = dmn0 to dmn1:
-        values = {'supplier': supplier,
-                'project': propject,
-                'year': year,
-                'letter': periode,
-                'period': f'{period}'
-                }
+year_end += 1
 
+snippets: str = ''
+for year in range(year_start, year_end):
+    for period in range(dmn0, dmn1):
+        values = {'supplier': supplier.lower(),
+                  'supplier_up': supplier.upper(),
+                  'project': project.lower(),
+                  'project_up': project.upper(),
+                  'year': year,
+                  'letter': periode,
+                  'period': f'{period:02d}'
+                 }
+
+        result = snippet.format(**values)
+        snippets += result
+
+snippet_name = os.path.join(config_dir, 'snippet-generate-delivery.txt')
+with open(snippet_name, 'w') as outfile:
+    outfile.write(snippets)
 
 
