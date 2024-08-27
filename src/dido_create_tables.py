c8016ff5c9eca76c1b5aacb54b12edd39f4217e3
arnold <arnold.reinders@minbzk.nl>
2024-08-27 13:54:01 +0200

    Custom SQL scripts enabled

--- src/dido_create_tables.py
+++ src/dido_create_tables.py
@@ -14,6 +14,7 @@ work directrory staat wordt altijd overschreven door dido_create_tables.
 """
 import os
 import sys
+import yaml
 import time
 import shutil
 import pandas as pd
@@ -1028,20 +1029,124 @@ def write_sql(meta_data: pd.DataFrame,
 
             # if
 
-            # add custom SQL to sql_code when present
-            if sql_script is not None:
-                with open(sql_script, 'r') as infile:
-                    logging.info(f'Adding custom SQL code from {sql_script}')
-
-
             outfile.write(sql_code)
             outfile.write('\n\n')
+
         # for -- tables
+
+        # add custom SQL to sql_code when present
+        if sql_script is not None:
+            extra_sql = apply_sql_script(
+                supplier_id = supplier_id,
+                project_name = project_name,
+                sql_script = sql_script,
+                servers = servers,
+            )
+
+            outfile.write(extra_sql)
+            outfile.write('\n\n')
+        # if
     # with
 
     return
 
 ### write_sql ###
+
+
+def apply_sql_script(supplier_id: str,
+                     project_name: str,
+                     sql_script: str,
+                     servers: dict,
+                    ) -> str:
+    """ Read custom SQL script.
+
+    Reads a custom SQL script yaml file and converts it to real SQL.
+
+
+    Args:
+        supplier_id (str): name of the supplier
+        project_name (str): name of the project
+        sql_script (str): dict containing the sql script instructions
+        servers (dict): server configurations
+
+    Raises:
+        DiDoError: when irrecoverable errors occur
+
+    Returns:
+        str: generated SQL
+    """
+    # read sql script as yaml config file
+    with open(sql_script, 'r') as infile:
+        script = yaml.safe_load(infile)
+
+    logger.info('')
+    logger.info(f'[Adding custom SQL code from {sql_script}]')
+
+    # test if required fields are in script
+    if 'AFFECTED_TABLES' not in script.keys() or \
+        'SQL' not in script.keys():
+
+        raise DiDoError('Script MUST contain AFFECTED_TABLES and SQL fields')
+
+    # if
+
+    # get all names of tables for this supplier and project
+    table_names = dc.get_table_names(project_name, supplier_id, 'description')
+    table_names = [table_names[k] for k in table_names.keys()]
+    data_names = dc.get_table_names(project_name, supplier_id, 'data')
+    [table_names.append(data_names[k]) for k in data_names.keys()]
+
+    # determine to which tables to apply the script
+    affected_tables = script['AFFECTED_TABLES']
+    if affected_tables == ['*']:
+        affected_tables = table_names
+
+    # test if affected tables exist
+    errors = []
+    for table_name in affected_tables:
+        if table_name not in table_names:
+            errors.append(table_name)
+
+    if len(errors) > 0:
+        raise DiDoError('*** Non existing affected tables: ' + str(errors))
+
+    # get popstgres schema for the tables: always in data server
+    server = servers['DATA_SERVER_CONFIG']
+    schema = server['POSTGRES_SCHEMA']
+
+    # get sql statements
+    statements = script['SQL']
+
+    # iterate over all tables, over all sql statements
+    logger.info('Tables affected:')
+    queries = ''
+    for table in affected_tables:
+        logger.info(f' - {table}')
+
+        for statement in statements:
+            # format variables:
+            # - schema: database schema
+            # - table: name of the table to apply
+            vars = {'schema': schema, 'table': table}
+            query = statements[statement].format(**vars)
+
+            # test for terminating ;
+            if not query.endswith(';'):
+                query += ';'
+
+            # # add qury to queries
+            queries += query + '\n'
+
+        # for
+
+    # for
+
+    # db_col = {'schema': schema, 'table': table}
+    # query = dc.DB_SHOWD.format(**db_col)
+
+    return queries
+
+### apply_sql_script ###
 
 
 def test_for_existing_tables(supplier_id: str,
@@ -2058,6 +2163,8 @@ def dido_create(config_dict: dict):
 
         # Read custom SQL script for supplier, None when not present
         sql_script = dc.get_par(config_dict, 'SQL_SCRIPT', None)
+        if sql_script is not None and not sql_script.startswith('/'):
+            sql_script = os.path.join(root_dir, 'sql', leverancier_id, sql_script)
 
         # Iterate over all projects
         for project_key in projects:
